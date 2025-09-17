96e1885a7bab7029ce53376e49866b5fb3841d55
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-09-17 20:29:56 +0530

    create home component

---
+++ src/component/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css"; // Optional: for custom styles
+
+function Home() {
+  return (
+    <section className="home-section">
+        <div className="container text-center">
+            
+            </div>
+    </section>
+  );
+}
+
+export default Home;
