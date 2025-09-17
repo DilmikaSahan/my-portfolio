8fa69971e8855de10777af97d15ceca19d775b3d
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-09-17 20:59:04 +0530

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home  from "./component/Home/Home";
-
 function App() {
   return (
     <div>
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,14 +1,20 @@
+
 import React from "react";
 import "./Home.css"; // Optional: for custom styles
+import NavBar from "../NavBar/NavBar";
+
 
 function Home() {
-  return (
-    <section className="home-section">
-        <div className="container text-center">
-            
-            </div>
-    </section>
-  );
+    return (
+        <>
+            <NavBar />
+            <section className="home-section">
+                <div className="container text-center">
+                    {/* Home page content goes here */}
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default Home;
